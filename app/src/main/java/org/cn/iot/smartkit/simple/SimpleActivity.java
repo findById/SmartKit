1ad678356360d7c6fa617bf6172fca9e69587eb6
findById <328559929@qq.com>
2017-03-28 18:38:44 +0800

    Initial commit

--- app/src/main/java/org/cn/iot/smartkit/simple/SimpleActivity.java
+++ app/src/main/java/org/cn/iot/smartkit/simple/SimpleActivity.java
@@ -5,10 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SwitchCompat;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -25,11 +28,23 @@ import org.cn.plugin.message.service.MessageService;
 public class SimpleActivity extends AppCompatActivity {
 
     private SwitchCompat mSwitchCompat;
+    private TextView mDeviceStatus;
+
+    private String deviceId = "";
+
+    private boolean online = false;
+
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
+
+        deviceId = getIntent().getStringExtra("deviceId");
+        if (TextUtils.isEmpty(deviceId)) {
+            deviceId = "ESP8266";
+        }
 
         registerReceiver(receiver, new IntentFilter(MessageActivity.ACTION_MESSAGE));
 
@@ -48,14 +63,29 @@ public class SimpleActivity extends AppCompatActivity {
         mSwitchCompat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MessageService.publish(mSwitchCompat.getContext(), "ESP8266", mSwitchCompat.isChecked() ? "051" : "050");
+                MessageService.publish(mSwitchCompat.getContext(), deviceId, mSwitchCompat.isChecked() ? "051" : "050");
             }
         });
+
+        mDeviceStatus = (TextView) findViewById(R.id.deviceStatus);
+        mDeviceStatus.setText(deviceId);
     }
 
     private void initData() {
-        MessageService.publish(this, "ESP8266", "1");
+        MessageService.publish(this, deviceId, "1");
+        mHandler.postDelayed(runnable, 1000 * 5);
     }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            if (online) {
+                mDeviceStatus.setText(deviceId);
+            } else {
+                mDeviceStatus.setText("设备离线");
+            }
+        }
+    };
 
     private BroadcastReceiver receiver = new BroadcastReceiver() {
 
@@ -65,6 +95,9 @@ public class SimpleActivity extends AppCompatActivity {
                 case MessageActivity.ACTION_MESSAGE: {
                     try {
                         Message message = (Message) intent.getSerializableExtra(MessageActivity.EXTRA_MESSAGE_DATA);
+                        if (message.producerId.equals(deviceId) || "ESP8266".equals(deviceId)) {
+                            online = true;
+                        }
                         JSONObject obj = JSON.parseObject(message.body);
                         if ("opened".equals(obj.getString("metadata"))) {
                             mSwitchCompat.setChecked(true);
